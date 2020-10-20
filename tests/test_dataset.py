e4b76605ba54f5ffbe5bcbc9fe611073c17387d6
ilinvalery <valery.ilin@skoltech.ru>
2020-10-20 10:25:54 +0300

    fix dataset, added new stractures

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,7 +2,7 @@ import unittest
 import gtsam
 import numpy as np
 from visual_gtsam.dataset import Dataset
-from visual_gtsam.dataset.structures import Image, Imu
+from visual_gtsam.dataset.structures import Image, Imu, Range, Transformation, Vector3D
 from datetime import datetime, timedelta
 import random
 import cv2
@@ -20,6 +20,18 @@ class TestDataset(unittest.TestCase):
         print("Delta between image and imu is (seconds)", image.get_time() - imu.get_time())
         self.assertAlmostEqual(image.get_time().get_time(), imu.get_time().get_time(), delta=timedelta(milliseconds=10))
         print(dataset.get_statistic())
+
+    def test_dataset_new_structures(self):
+        dataset = Dataset()
+        self.assertIsInstance(dataset.get_next_range(), Range)
+        self.assertIsInstance(dataset.get_next_ur(), Transformation)
+        range = dataset.get_next_range()
+        print(range)
+        self.assertIsInstance(range.get_value(), float)
+        transform = dataset.get_next_ur()
+        print(transform)
+        self.assertIsInstance(transform.get_rotation(), np.ndarray)
+
 
     def test_collect_images_for_dataset(self):
         dataset = Dataset()
--- visual_gtsam/dataset/dataset.py
+++ visual_gtsam/dataset/dataset.py
@@ -2,20 +2,29 @@ import os
 from google_drive_downloader import GoogleDriveDownloader as gdd
 from datetime import timedelta
 
-from visual_gtsam.dataset.structures import ImuSequence, ImageSequence, Image, Imu
+from visual_gtsam.dataset.structures import ImuSequence, ImageSequence, Image, Imu, RangeSequence, Range, Transformation
+from visual_gtsam.dataset.structures.ur_sequence import URSequence
 
 
 class Dataset(object):
     _image_sequence: ImageSequence
     _imu_sequence: ImuSequence
+    _range_sequence: RangeSequence
+    _ur_sequence: URSequence
 
-    def __init__(self, file_id='1XyRTmIfan7irlVnXYPh_M-2KgJLNDpl_', main_dir='downloads',
-                 imu_json_filename='imus_data.json', image_dir='images', image_json_filename='image_data.json'):
+    def __init__(self, file_id='1aZsZEuSPJy-WOcQ9Y2F-rahl-7J0Bko_', main_dir='downloads',
+                 imu_json_filename='imus_data.json',
+                 ur_json_filename='ur_data.json',
+                 range_json_filename='range_data.json',
+                 image_dir='images', image_json_filename='image_data.json'):
         self._file_id = file_id
         self._main_dir = main_dir
+
+        self._ur_json_filename = ur_json_filename
+        self._range_json_filename = range_json_filename
         self._imu_json_filename = imu_json_filename
-        self._image_dir = image_dir
         self._image_json_filename = image_json_filename
+        self._image_dir = image_dir
 
         self._is_downloaded = False
         self._download_dataset(file_id)
@@ -38,6 +47,8 @@ class Dataset(object):
 
     def _read_dataset(self):
         self._imu_sequence = ImuSequence(self._main_dir, self._imu_json_filename)
+        self._range_sequence = RangeSequence(self._main_dir, self._range_json_filename)
+        self._ur_sequence = URSequence(self._main_dir, self._ur_json_filename)
         self._image_sequence = ImageSequence(self._main_dir, self._image_dir, self._image_json_filename)
 
     def get_image_sequence(self) -> ImageSequence:
@@ -46,10 +57,21 @@ class Dataset(object):
     def get_imu_sequence(self):
         return self._imu_sequence
 
+    def get_range_sequence(self):
+        return self._range_sequence
+
+    def get_ur_sequence(self):
+        return self._ur_sequence
+
     def get_statistic(self):
-        msg = "Total amount of IMU values is {}\nTotal amount of Images is {}".format(self._imu_sequence.get_length(),
-                                                                                      self._image_sequence.get_length())
-        return msg
+        return "Total amount of IMU values is {}\n" \
+               "Total amount of Images is {}\n" \
+               "Total amount of UR values {}\n" \
+               "Total amount of range values {}\n" \
+            .format(self._imu_sequence.get_length(),
+                    self._image_sequence.get_length(),
+                    self._range_sequence.get_length(),
+                    self._ur_sequence.get_length())
 
     def get_imu_statistic(self):
         return self._imu_sequence.get_all_velocity(), self._imu_sequence.get_all_acceleration()
@@ -59,6 +81,12 @@ class Dataset(object):
 
     def get_next_imu(self) -> Imu:
         return self._imu_sequence.get_next()
+
+    def get_next_range(self) -> Range:
+        return self._range_sequence.get_next()
+
+    def get_next_ur(self) -> Transformation:
+        return self._ur_sequence.get_next()
 
     def get_next_pair(self) -> (Image, Imu):
         image = self.get_next_image()
@@ -73,3 +101,5 @@ class Dataset(object):
     def reset(self):
         self._imu_sequence.reset()
         self._image_sequence.reset()
+        self._range_sequence.reset()
+        self._ur_sequence.reset()
